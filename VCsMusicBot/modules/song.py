aa60712afe253e8d34bef985f83e7292655d8d72
@ASHIRU MALSHAN <79177943+ASHIRUMALSHAN@users.noreply.github.com>
2021-06-30 01:05:35 +0530

    Update song.py

--- VCsMusicBot/modules/song.py
+++ VCsMusicBot/modules/song.py
@@ -59,7 +59,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵Song Name:** `{thum}` \n**🎤Requested For:** `{urlissed}` \n**🔔Channel:** `{thums}` \n**🔗Link:** `{mo}` \n**📤Uploaded By: @UNLIMITEDworldTEAM**"
+        rep = "**🎵Song Name:** `{title}` \n**🎤Requested For:** `{thumb}` \n**🔔Channel:** `{results}` \n**🔗Link:** `{link}` \n**📤Uploaded By: @UNLIMITEDworldTEAM**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
